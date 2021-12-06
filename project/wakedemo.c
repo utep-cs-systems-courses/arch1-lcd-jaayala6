a29d94e14c2dcdcbe928ccceb13303236c25c7d7
jaayala6 <jaayala6@miners.utep.edu>
2021-12-06 15:18:52 -0700

    bounce control

--- project/wakedemo.c
+++ project/wakedemo.c
@@ -50,29 +50,44 @@ void main()
 void
 update_shape()
 {
+
   static unsigned char row = screenHeight / 2, col = screenWidth / 2;
   static char blue = 31, green = 0, red = 31;
+  static char blue2 = 31, green2 = 0, red2 = 31;
   unsigned int color = (blue << 11) | (green << 5) | red;
   unsigned int color2 = (blue << 9) | (green << 2) | red;
+  unsigned int color3 = (blue2 << 3) | (green2 << 7) | red2;
   static unsigned char step = 0;
   static unsigned char bottomEdge = 159;
-  static unsigned char topEdge = 0;
-  static unsigned char sideEdge = 127;
-  static unsigned char xspeed = 1;
-  static unsigned char yspeed = 1;
+  static unsigned char left_topEdge = 0;
+  static unsigned char rightEdge = 115;
+  static unsigned char xspeed = 3;
+  static unsigned char yspeed = 3;
   static unsigned char x = 0;
-  static unsigned char y = 80;
-
+  static unsigned char y = 81;
+  
   if (switches & SW4) return;
-  clearScreen(COLOR_BLACK);
-  drawString5x7(x, y, "DVD", color, color2);
-  if (switches & SW3) green = (green + 1) % 64;
-  if (switches & SW2) blue = (blue + 2) % 32;
-  if (switches & SW1) red = (red - 3) % 32;
+  //clearScreen(COLOR_BLACK);
+  fillRectangle(x, y-1, 18, 10, color3);
+  drawString5x7(x, y, "DVD", color, color3);
+  
   x = x + xspeed;
   y = y + yspeed;
-  if (x + 5 == sideEdge || x == 0) {
+  if (switches & SW3){
+    green = (green + 1) % 64;
     xspeed = xspeed * -1;
+  }
+  if (switches & SW2) {
+    blue = (blue + 2) % 32;
+    yspeed = yspeed * -1;
+  }
+  if (switches & SW1) red = (red - 3) % 32;
+
+  if (x + 5 >= rightEdge || x == left_topEdge) {
+    xspeed = xspeed * -1;
+    green2 = (green2 + 1) % 64;
+    blue = (blue2 + 2) % 32;
+    red = (red2 - 3) % 32;
     //int startCol = col - step;
     //int endCol = col + step;
     //int width = 1 + endCol - startCol;
@@ -85,7 +100,10 @@ update_shape()
     //drawString5x7(x, y, "DVD", color, color2);
     //step ++;
   }
-  if (y + 7 == bottomEdge || y == 0) {     
+  if (y + 7 >= bottomEdge || y == left_topEdge) {     
+    green2 = (green + 1) % 64;
+    blue2 = (blue + 2) % 32;
+    red2 = (red - 3) % 32;
     yspeed = yspeed * -1;
   }
 }
